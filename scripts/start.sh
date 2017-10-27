72acae2e6c050c64753fcc4663d306457236539e
Bob Tao <taobo22@hotmail.com>
2017-10-27 09:40:04 +0800

    Fix pac bugs

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@ if [ "$role" = "server" ];then
     ss-server -u -c $conf --plugin obfs-server --plugin-opts "obfs=http" "${@:2}"
 elif [ "$role" = "proxy" ];then
     polipo socksParentProxy=localhost:7654 proxyAddress="0.0.0.0" &
-    niginx
+    nginx
     conf="${projecthome}/conf.d/ss.client.config.json"
     pluginopts="obfs=http;obfs-host=www.apple.com" 
     ss-local -u -c $conf --plugin obfs-local --plugin-opts $pluginopts "${@:2}"
